9f4dcdb7a775643b4b3fe4bf260f26d12080c8b0
Miguel Ortega <omiguel1992@gmail.com>
2020-04-08 18:10:47 -0400

    Types examples

---
+++ 01.Types/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// Types
+var framework = 'Angular';
+var isProduction = true;
+var PORT = 3000;
+var styles = ['sasas'];
+var sayHello = function (name) { return "Hello " + name; };
+var value = sayHello('Miguel');
+console.log(value);
---
+++ 01.Types/index.ts
@@ -0,0 +1,9 @@
+// Types
+const framework: string = 'Angular';
+const isProduction: boolean = true;
+const PORT: number = 3000;
+const styles: Array<string> = ['sasas'];
+const sayHello: Function = (name: string) => `Hello ${name}`;
+
+const value = sayHello('Miguel');
+console.log(value);
